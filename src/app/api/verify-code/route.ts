284e0fb0ddfa261f0979107ab8ed088ed2d793f9
Aavash Paudel <muproject990@gmail.com>
2024-05-07 18:27:22 +0545

    yes done

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,69 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, code } = await request.json();
+
+    const decodedUsername = decodeURIComponent(username);
+    //space is changed inti %20
+
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not Found",
+        },
+        { status: 500 }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeNotExpired && isCodeValid) {
+      user.isVerified = true;
+      await user.save();
+
+      return Response.json(
+        {
+          success: true,
+          message: "Account Verified sucesfully",
+        },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verification Code has Expired, please signup again to get a new code",
+        },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Incorrect Verification code",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+  } catch (error) {
+    console.error("Error verifying user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error verifying user",
+      },
+      { status: 500 }
+    );
+  }
+}
--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,30 +1,23 @@
 import mongoose from "mongoose";
 
 type ConnectionObject = {
-    isConnected?:number
-}
+  isConnected?: number;
+};
 
+const connection: ConnectionObject = {};
 
+export default async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already Connectd to DB");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(`${process.env.MONGODB_URI}`);
+    connection.isConnected = db.connections[0].readyState;
 
-const connection: ConnectionObject = {}
-
-export default async function dbConnect():Promise<void> {
-    if (connection.isConnected)  {
-        console.log("Already Connectd to DB");
-        return        
-    }
-    try {
-        const db=await mongoose.connect(process.env.MONGODB_URI ||'',{})
-        connection.isConnected = db.connections[0].readyState
-
-        console.log("DB Connected Sucessfully");
-
-        
-        
-    } catch (error) {
-        console.log("Database Conn Failed",error);
-        process.exit(1)
-        
-    }
-    
+    console.log("DB Connected Sucessfully");
+  } catch (error) {
+    console.log("Database Conn Failed erro 1", error);
+    process.exit(1);
+  }
 }
